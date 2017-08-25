7b56d813c03fd6fd82af842588725509c0467044
Acoyan <yangabriel2000@gmail.com>
2017-08-25 09:27:14 -0300

    Adicionar aquivos

---
+++ Assets/CustomList.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic; 
+
+public class CustomList : MonoBehaviour {
+
+
+	[System.Serializable]
+	public class Entry {
+		public string key; public int value;
+	}
+
+
+	public List<Entry> MyList = new List<Entry>(1);
+
+
+	void Reset(){
+		
+		MyList.Add(new Entry());
+		MyList.Add(new Entry());
+	}
+
+
+}
---
+++ Assets/Editor/CustomListEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(CustomList))]
+
+public class CustomListEditor : Editor {
+
+
+	public override void OnInspectorGUI(){
+		CustomList X = target as CustomList; 
+
+
+
+
+		for(int i = 0; i < X.MyList.Count; i++)
+		{
+			X.MyList [i].value = EditorGUILayout.IntField ("papai", X.MyList [i].value);
+			
+		}
+}
+}
+
